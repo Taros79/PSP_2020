b43aab024b96d52eefe4f45f871135b64d8cb721
amanterechoncho79 <amanterechoncho79@gmail.com>
2021-11-12 00:00:13 +0100

    Acabado servlet

---
+++ MiPrimeraWeb/src/main/java/EE/constantes.java
@@ -0,0 +1,7 @@
+package EE;
+
+public class constantes {
+    public static final String SERVLET_PINTURA = "/ServletPintura";
+    public static final String TEXT_HTML = "text/html";
+    public static final String COLOR_ELEGIDO = "colorElegido";
+}
--- MiPrimeraWeb/src/main/java/EE/servlet/ServletPrincipal.java
+++ MiPrimeraWeb/src/main/java/EE/servlet/ServletPrincipal.java
@@ -1,7 +1,7 @@
 package EE.servlet;
 
+import EE.constantes;
 import com.github.javafaker.Faker;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,14 +10,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "Servlet", value = "/ServletPintura")
+@WebServlet(name = "Servlet", value = constantes.SERVLET_PINTURA)
 public class ServletPrincipal extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Faker f = new Faker();
-        response.setContentType("text/html");
+        response.setContentType(constantes.TEXT_HTML);
         PrintWriter pw = response.getWriter();
-        String color = request.getParameter("colorElegido");
+        String color = request.getParameter(constantes.COLOR_ELEGIDO);
         String c = "<body BGCOLOR=" + f.color().name() + ">";
         if (color.equals("Rojo"))
             pw.println("<body BGCOLOR=#ff000>");
@@ -27,19 +28,12 @@ public class ServletPrincipal extends HttpServlet {
             pw.println("<body BGCOLOR=blue>");
         if (color.equals("Amarillo"))
             pw.println("<body BGCOLOR=yellow>");
-        if (color.equals("Negro")) {
+        if (color.equals("Negro"))
             pw.println("<body BGCOLOR=black>");
-        }
-        if (color.equals("Aleatorio")) {
+        if (color.equals("Random"))
             pw.println(c);
-        }
         pw.println("<center><h2>El color seleccionado fue: " + color + "</h2></center>");
-        ;
         pw.close();
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
