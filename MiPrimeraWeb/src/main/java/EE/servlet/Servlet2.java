7b574d44ea150a4a318ff9d5e3faf3a7ad108e61
amanterechoncho79 <amanterechoncho79@gmail.com>
2021-12-01 13:26:32 +0100

    Acabado servlet 2.0 colores

---
+++ MiPrimeraWeb/src/main/java/EE/servlet/Servlet2.java
@@ -0,0 +1,27 @@
+package EE.servlet;
+
+import EE.Constantes;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "Servlet2", value = "/Servlet2")
+public class Servlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType(Constantes.TEXT_HTML);
+        var colorServletPintura = request.getSession().getAttribute("color");
+        response.getWriter().println("<center><body bgcolor=" + colorServletPintura + "></body></center>");
+        response.getWriter().close();
+        request.getSession().setAttribute("color", null);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
--- MiPrimeraWeb/src/main/java/EE/servlet/ServletPrincipal.java
+++ MiPrimeraWeb/src/main/java/EE/servlet/ServletPrincipal.java
@@ -1,39 +1,25 @@
 package EE.servlet;
 
 import EE.Constantes;
-import com.github.javafaker.Faker;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "Servlet", value = Constantes.SERVLET_PINTURA)
 public class ServletPrincipal extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Faker f = new Faker();
-        response.setContentType(Constantes.TEXT_HTML);
-        PrintWriter pw = response.getWriter();
-        String color = request.getParameter(Constantes.COLOR_ELEGIDO);
-        String c = "<body BGCOLOR=" + f.color().name() + ">";
-        if (color.equals("Rojo"))
-            pw.println("<body BGCOLOR=#ff000>");
-        if (color.equals("Verde"))
-            pw.println("<body BGCOLOR=green>");
-        if (color.equals("Azul"))
-            pw.println("<body BGCOLOR=blue>");
-        if (color.equals("Amarillo"))
-            pw.println("<body BGCOLOR=yellow>");
-        if (color.equals("Negro"))
-            pw.println("<body BGCOLOR=black>");
-        if (color.equals("Random"))
-            pw.println(c);
-        pw.println("<center><h2>El color seleccionado fue: " + color + "</h2></center>");
-        pw.close();
+        var colIndex = request.getParameter(Constantes.COLOR_ELEGIDO);
+        request.getSession().setAttribute("color", colIndex);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
